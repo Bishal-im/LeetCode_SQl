0dff4422fdf021cd49cb231686a27fc6d6b1caf9
Bishal Pandey <bishalpandey.im@gmail.com>
2025-11-16 23:15:00 +0545

    Leetcode problem solution 177

---
+++ 177_Nth_Highest_salary.sql
@@ -0,0 +1,11 @@
+CREATE FUNCTION getNthHighestSalary(N INT) RETURNS INT
+BEGIN
+ set N=N-1;
+  RETURN (
+      Select Distinct salary 
+      from Employee
+      order by salary 
+      Limit 1 offset N
+
+  );
+END
